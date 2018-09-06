b6b0afca5bb0f6124653b93232e1f7f0f05d9514
guitarbien <guitarbien@gmail.com>
2018-09-06 23:43:19 +0800

    tw zip select

---
+++ src/components/TwZipSelect.vue
@@ -0,0 +1,28 @@
+<template>
+  <label>
+    <select v-model="optionValue" @change="sendingEvent">
+      <option v-for="(item, key) in list" :key="key" :value="key">{{ item.name }}</option>
+    </select>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'MySelect',
+  props: ['list'],
+  data() {
+    return {
+      optionValue: 0,
+    };
+  },
+  methods: {
+    sendingEvent() {
+      this.$emit('parent-change-city', this.optionValue);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
